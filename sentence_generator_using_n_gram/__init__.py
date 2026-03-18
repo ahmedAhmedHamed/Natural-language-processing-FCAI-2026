3a7228d442341ba1f469eae2842bb8a062a6b1a8
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2026-03-19 00:05:16 +0200

    init now initialises nltk with the needed dependencies

---
+++ sentence_generator_using_n_gram/__init__.py
@@ -0,0 +1,8 @@
+import nltk
+
+from preprocessing import run_preprocessing
+nltk.download() # NLTK Package Manager
+nltk.download('punkt') # for tokenization
+nltk.download('wordnet') # for lemmatization
+#%%
+
