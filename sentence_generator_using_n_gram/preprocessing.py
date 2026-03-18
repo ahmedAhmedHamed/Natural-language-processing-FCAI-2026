d2c33449de450795c0d0a2d6452bb6ef9d1ef26a
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2026-03-19 00:17:41 +0200

    implement vocabulary set creation

--- sentence_generator_using_n_gram/preprocessing.py
+++ sentence_generator_using_n_gram/preprocessing.py
@@ -25,17 +25,20 @@ def convert_all_tokens_to_lower_case(tokens: List[str]) -> List[str]:
     tokens = [token.lower() for token in tokens]
     return tokens
 
-def build_a_set_of_vocabulary_from_pre_processed_corpus(tokens: List[str]):
+def build_a_set_of_vocabulary_from_pre_processed_corpus(tokens: List[str]) -> set(str):
     """step five"""
-    pass
+    vocabulary_set = set(tokens)
+    return vocabulary_set
 
 def run_preprocessing(sentences: str):
     tokens = tokenize_sentences_into_words(sentences)
     tokens = remove_punctuation_from_tokens(tokens)
     tokens = remove_stop_words(tokens)
     tokens = convert_all_tokens_to_lower_case(tokens)
-    vocab_set = build_a_set_of_vocabulary_from_pre_processed_corpus(tokens)
-    return vocab_set
+    # doing the set step as the first step would be faster, but I am doing this to be faithful to the assignment description
+    # - Ahmed
+    vocabulary_set = build_a_set_of_vocabulary_from_pre_processed_corpus(tokens)
+    return vocabulary_set
 
 if __name__ == '__main__':
     print(string.punctuation)
