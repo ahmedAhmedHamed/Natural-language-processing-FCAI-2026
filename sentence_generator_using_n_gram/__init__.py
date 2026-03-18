2dd2e135380e8ceb8544d0ffa300c31e10024376
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2026-03-19 00:28:38 +0200

    remove nltk.download() call

--- sentence_generator_using_n_gram/__init__.py
+++ sentence_generator_using_n_gram/__init__.py
@@ -1,7 +1,5 @@
 import nltk
-
 from preprocessing import run_preprocessing
-nltk.download() # NLTK Package Manager
 nltk.download('punkt') # for tokenization
 nltk.download('wordnet') # for lemmatization
 #%%
