c800a088effd32847eacb405742a7c5022cc5296
Noran-Mahmoud <noran.m.ismail05@gmail.com>
2026-03-19 09:44:23 +0200

    Second step done

--- sentence_generator_using_n_gram/preprocessing.py
+++ sentence_generator_using_n_gram/preprocessing.py
@@ -1,16 +1,15 @@
-"""
-all functions can be used independently, but only run_preprocessing should be used in this assignment.
-"""
-
 import string
 from typing import List
 import nltk
 from nltk.corpus import stopwords
+from nltk.corpus import brown
 
 nltk.download('punkt')  # for tokenization
 nltk.download('stopwords')  # for lemmatization
 nltk.download('punkt_tab')
+nltk.download('brown')
 
+##################### Preprocessing #####################
 
 def tokenize_sentences_into_words(sentences: str) -> List[str]:
     """step one"""
@@ -51,9 +50,50 @@ def run_preprocessing(sentences: str):
     tokens = build_a_set_of_vocabulary_from_pre_processed_corpus(tokens)
     return tokens
 
+
+##################### Dictionary Building #####################
+
+def get_tokens_from_brown(limit=200_000):
+    tokens = []
+    
+    for sentence in brown.sents():
+        tokens.extend(sentence)
+        
+        if len(tokens) >= limit:
+            break
+    
+    return tokens[:limit]
+
+def build_ngram_dict(corpus, n):
+    tokens = remove_punctuation_from_tokens(corpus)
+    tokens = convert_all_tokens_to_lower_case(tokens)
+    # brown corpus is already tokenized
+    # the set step is affecting the n-gram generation order-wise, so I discarded it, besides there is no need for uniqueness in the n-gram
+    # and for the stop words, it was canceled in the last remarks ¯\_(ツ)_/¯
+    # - Noran
+    ngrams = {}
+
+    for i in range(len(tokens) - n + 1):
+        gram = tuple(tokens[i:i+n])
+        
+        if gram in ngrams:
+            ngrams[gram] += 1
+        else:
+            ngrams[gram] = 1
+
+    return ngrams
+
 if __name__ == '__main__':
 
     example = "The quick brown fox jumps over the lazy dog, and the dog barked loudly!"
     print('before: --- :', example)
     tokenized_example = run_preprocessing(example)
     print('after: --- :', tokenized_example)
+
+    brown_tokens = get_tokens_from_brown()
+    print(f"\n-> Total tokens from Brown corpus: {len(brown_tokens)}")
+    trigrams = build_ngram_dict(brown_tokens, 3)
+    print(f"-> Dictionary: {len(trigrams)} n-grams found")
+    for k, v in trigrams.items():
+        if(v > 20):     # to avoid large number of n-grams
+            print(k, ":", v)
